26925eb35c5b88d5d39c88317cc7b1ff97a66819
ShiryAbdo <shaimaaenfikry@gmail.com>
2017-09-27 17:06:34 +0200

    mm

--- app/src/main/java/governmental/service/egypt/GetData.java
+++ app/src/main/java/governmental/service/egypt/GetData.java
@@ -2,15 +2,50 @@ package governmental.service.egypt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import governmental.service.egypt.data.User;
 
 public class GetData extends AppCompatActivity {
-    private DatabaseReference mDatabase;
+      DatabaseReference mDatabase;
+      TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_data);
+        textView=(TextView)findViewById(R.id.textView);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        User user = new User("shimaa","email", "pass");
+
+        mDatabase.child("users").child("shimaa").setValue(user);
+        mDatabase.child("users").child("shimaa").addValueEventListener (new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    User post = postSnapshot.getValue(User.class);
+                    String username = post.getUsername();
+                    textView.setText( username);
+
+
+//               if (username.equals(mUserView.getText().toString())) {
+//                   alreadyRegisteredAccount++;
+//               }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
